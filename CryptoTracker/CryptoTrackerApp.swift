c023b7c0ae4799194348f090b7a2a3b50dd09773
Aleksandra <aleksandrahankevic2@gmail.com>
2022-06-06 15:33:35 +0300

    Initial commit && Color setup

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Aleksandra on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
